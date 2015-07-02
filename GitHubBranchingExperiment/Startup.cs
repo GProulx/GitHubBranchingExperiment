e07d803b88510dfa0bbe3caae7c8ec85b2622240
Ghislain Proulx <gproulx@ghislainproulx.net>
2015-07-02 18:00:50 -0400

    Initial commit

---
+++ GitHubBranchingExperiment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubBranchingExperiment.Startup))]
+namespace GitHubBranchingExperiment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
